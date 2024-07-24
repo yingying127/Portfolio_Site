88cee2a0ac2f82176e64446a8fd3de74ae1d21f9
Ying Ying <yingyingfengg@gmail.com>
2024-07-24 17:51:43 -0400

    detecting user

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+if (
+  (window.performance && window.performance.navigation.type === window.performance.navigation.TYPE_BACK_FORWARD) ||
+  (document.referrer.indexOf("https://yingying-feng.com/") >= 0)
+) {
+  console.log('hey there!');
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
